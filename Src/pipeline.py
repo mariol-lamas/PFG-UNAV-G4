0153033e743129fea5af65cb4c2da35cef5e6f15
mariol-lamas <mario.lamas.herrera@gmail.com>
2024-04-11 00:22:31 +0200

    Seleccion de variables (Falta terminar

---
+++ Src/pipeline.py
@@ -0,0 +1,13 @@
+# Importacion de paquetes
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+
+
+class DataProcessor():
+    def __init__(self,ruta_data_raw) -> None:
+        pass
+        
